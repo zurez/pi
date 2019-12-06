042dab6b83cb0abdfc6e3f1acade5ba043df1610
zurez <zurez4u@gmail.com>
2019-12-06 23:11:16 +0530

    ff

--- templates/script.js
+++ templates/script.js
@@ -1,4 +1,4 @@
-var socket = io("http://127.0.0.1:5000");
+var socket = io("http://192.168.1.92:5000");
 socket.on('connect', function() {
     socket.emit('my event', {data: 'I\'m connected!'});
 });
