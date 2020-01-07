505304cd518645a9e23da27dba6b188a81ce648f
zurez <zurez4u@gmail.com>
2020-01-08 00:32:42 +0530

    video fixes

--- video_feed.py
+++ video_feed.py
@@ -37,7 +37,7 @@ ser = serial.Serial("/dev/ttyAMA0",9600, 8, 'N', 1, timeout=1)
 # initialize the video stream and allow the camera sensor to
 # warmup
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0,framerate = 30).start()
+vs = VideoStream(src=-1,framerate = 30).start()
 print(vs)
 eventlet.sleep(2.0)
 print("[INFO] started...")
