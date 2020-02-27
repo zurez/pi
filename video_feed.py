cd47a828f0dda2211cbc7c6c16f794fabb0b095f
Zurez <zurez4u@gmail.com>
2020-02-27 17:52:38 +0530

    fs

--- video_feed.py
+++ video_feed.py
@@ -223,7 +223,8 @@ def gas():
 def video_record_handler():
     global recording
     print("video_recording")
-
+    fileName = 'recordings/'+str(time.time())+'.avi'
+    
     if recording == True:
         # Pause or Stop
         print("pausing the video")
@@ -232,9 +233,11 @@ def video_record_handler():
         # cap.release()
         out.release()
         cv2.destroyAllWindows()
+        
     else:
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('recordings/'+str(time.time())+'.avi',fourcc, 20.0, (640,480))
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(fileName,fourcc, 20.0, (640,480))
+    
         recording = True
     return 'success'
         
